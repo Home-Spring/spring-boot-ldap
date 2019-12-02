d500efe2eba6b57bf2d38ea50b994af90c7746be
Lazarchuk <a.lazarchuk@cartsys.com.ua>
2019-12-02 15:23:42 +0200

    #auth-user-ad.right  added: ADLdap Provider

---
+++ auth-user-ad.right/src/main/java/dk/digitalidentity/Application.java
@@ -0,0 +1,68 @@
+package dk.digitalidentity;
+
+import dk.digitalidentity.app.config.ADLdapConfig;
+import dk.digitalidentity.app.service.ADLdapService;
+import dk.digitalidentity.provider.ADLdapProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import dk.digitalidentity.app.data.ADLdap;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	@Autowired
+    private ADLdapService adLdapService;
+
+	@Autowired
+	private ADLdapProvider adLdapProvider;
+
+    public void run(String... args) {
+        boolean isAuthenticate = adLdapService.authenticate("", "user2", "Qwerty12");
+		System.out.println("(user2) AUTHENTICATE: " + isAuthenticate); //TODO:  authenticate = false     LDAP: error code 49 - comment: AcceptSecurityContext error, data 52e
+
+		isAuthenticate = adLdapService.authenticate("CN=Users2", "user1", "Qwerty1");
+		System.out.println("(user1 in CN=Users2) AUTHENTICATE: " + isAuthenticate); //TODO:  authenticate = false     LDAP: error code 32 - problem 2001 (NO_OBJECT)
+
+		isAuthenticate = adLdapService.authenticate(ADLdapConfig.BASE_USERDIR, "user1", "Qwerty1");
+		System.out.println("(user1 in " + ADLdapConfig.BASE_USERDIR + ") AUTHENTICATE: " + isAuthenticate); //TODO:  authenticate = true
+
+		System.out.println("|||||||||||||||||||||||||||");
+		if (isAuthenticate) {
+			System.out.println("1) Search All Users:\n----------------");
+			for (ADLdap group : adLdapProvider.allUsers()) System.out.println(group.getCn());
+
+			System.out.println("\n3) Search All Groups:\n----------------");
+			for (ADLdap group : adLdapProvider.allGroups()) System.out.println(group.getCn());
+
+			System.out.println("\n2) Search User by Name=user2:\n----------------");
+			for (ADLdap person : adLdapProvider.findUser("user2")) System.out.println(person.getCn());
+
+			System.out.println("\n4) Search Group(s) by UserName=user1:\n----------------");
+			for (ADLdap group : adLdapProvider.findGroups("user1", ADLdapConfig.BASE_USERDIR)) System.out.println(group.getCn());
+
+			System.out.println("\n5.1) * Search Group(s) by UserName=user2 in OU=ctsuser:\n----------------");
+			for (ADLdap group : adLdapProvider.findGroups("user2", ADLdapConfig.BASE_USERDIR, ADLdapConfig.ROLE_NAMES2)) System.out.println(group.getCn());
+
+			System.out.println("\n5.2) * Search Group(s) by UserName=user2 in OU=Ctsprog:\n----------------");
+			for (ADLdap group : adLdapProvider.findGroups("user2", ADLdapConfig.BASE_USERDIR, ADLdapConfig.ROLE_NAMES1)) System.out.println(group.getCn());
+
+			System.out.println("\n6) * Search Group(s) by UserName=user4 in OU=Ctsprog:\n----------------");
+			for (ADLdap group : adLdapProvider.findGroups("user4", ADLdapConfig.BASE_USERDIR, ADLdapConfig.ROLE_NAMES1)) System.out.println(group.getCn());
+		}
+		System.out.println("|||||||||||||||||||||||||||");
+
+////        String ROOT_DIR = "";                            //TODO:  user1
+////        String ROOT_DIR = "DC=adcts";                    //TODO:  user1@adcts
+//        String ROOT_DIR = "DC=adcts,DC=local";           //TODO:  user1@adcts.local
+////        String ROOT_DIR = "DC=blabla,DC=adcts,DC=local"; //TODO:  user1@blabla.adcts.local
+//
+//        System.out.println("UserDn = " + getUserDn("user1", ADLdapConfig.ROOT_DIR));
+	}
+
+    public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ auth-user-ad.right/src/main/java/dk/digitalidentity/provider/ADLdapProvider.java
@@ -0,0 +1,52 @@
+package dk.digitalidentity.provider;
+
+import dk.digitalidentity.app.config.ADLdapConfig;
+import dk.digitalidentity.app.data.ADLdap;
+import dk.digitalidentity.app.service.ADLdapService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ldap.filter.AndFilter;
+import org.springframework.ldap.filter.EqualsFilter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ADLdapProvider {
+
+    @Autowired
+    private ADLdapService adLdapService;
+
+    @Deprecated
+    public List<ADLdap> allUsers() {
+        AndFilter andFilter = new AndFilter();
+        andFilter.and(new EqualsFilter("objectclass", "person"));
+        return adLdapService.getAll(andFilter);
+    }
+
+    @Deprecated
+    public List<ADLdap> allGroups() {
+        AndFilter andFilter = new AndFilter();
+        andFilter.and(new EqualsFilter("objectclass", "group"));
+        return adLdapService.getAll(andFilter);
+    }
+
+    @Deprecated
+    public List<ADLdap> findUser(String userName) {
+        AndFilter andFilter = new AndFilter();
+        andFilter.and(new EqualsFilter("sAMAccountName", userName));
+        return adLdapService.getAll(andFilter);
+    }
+
+    public List<ADLdap> findGroups(String userName, String baseUserDir, String roleNames) {
+        AndFilter filter = new AndFilter();
+        filter.and(new EqualsFilter("member", "CN=" + userName + "," + baseUserDir + "," + ADLdapConfig.ROOT_DIR));
+        return adLdapService.getAll(roleNames, filter);
+    }
+
+    @Deprecated
+    public List<ADLdap> findGroups(String userName, String baseUserDir) {
+        AndFilter filter = new AndFilter();
+        filter.and(new EqualsFilter("member", "CN=" + userName + "," + baseUserDir + "," + ADLdapConfig.ROOT_DIR));
+        return adLdapService.getAll(filter);
+    }
+}
